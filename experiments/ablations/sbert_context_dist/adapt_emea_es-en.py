ba22fb1b877f2813131098f48b69a0ff8171aa37
authoranonymous321 <authoranonymous321@gmail.com>
2022-06-20 22:43:05 +0200

    SeqDist objectives: weights

--- experiments/ablations/sbert_context_dist/adapt_emea_es-en.py
+++ experiments/ablations/sbert_context_dist/adapt_emea_es-en.py
@@ -69,7 +69,7 @@ train_obj = SeqBertScoreObjective(lang_module,
                                   target_lang_id=tgt_lang,
                                   batch_size=1,
                                   objective_id=train_dataset_id,
-                                  loss_weight=250,
+                                  loss_weight=100,
                                   remember_last_input=True)
 
 # validations are also computed by the training MLE objective
