8a549e06090cea503cc3262f3aa308ad77ef8d1b
authoranonymous321 <authoranonymous321@gmail.com>
2022-06-21 09:51:09 +0200

    sbert_dec: DGT: weights

--- experiments/sbert_dec/adapt_dgt_en-de.py
+++ experiments/sbert_dec/adapt_dgt_en-de.py
@@ -70,7 +70,7 @@ train_obj = DeconSeqBertScoreObjective(lang_module,
                                        target_lang_id=tgt_lang,
                                        batch_size=1,
                                        objective_id=train_dataset_id,
-                                       loss_weight=100,
+                                       loss_weight=1,
                                        remember_last_input=True)
 
 # validations are also computed by the training MLE objective
