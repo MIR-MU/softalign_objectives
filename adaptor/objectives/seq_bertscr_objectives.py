ef7380a905416794c3698a9c30b9003bb80bc866
authoranonymous321 <authoranonymous321@gmail.com>
2022-06-21 10:21:41 +0200

    Random sbert: lightening

--- adaptor/objectives/seq_bertscr_objectives.py
+++ adaptor/objectives/seq_bertscr_objectives.py
@@ -555,7 +555,12 @@ class DeconSeqBertScoreObjectiveDistLoss(DeconSeqBertScoreObjective):
         return torch.vstack(losses).mean()
 
 
-class SeqBertScoreRandom(SeqBertScoreObjective):
+class SeqBertScoreRandom(Sequence2Sequence):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def _compute_loss(self,
                       inputs: Optional[Union[BatchEncoding, Dict[str, torch.Tensor]]] = None,
